d8ef6bbe8db18958e324c0e15f91d899e7419dc0
berserkcoder <shreyanshagrawalae2@gmail.com>
2025-08-15 17:22:13 +0530

    utils code construction

--- Project/src/index.js
+++ Project/src/index.js
@@ -9,3 +9,16 @@ dotenv.config({
 })
 
 connectDB()
+.then(()=>{
+    app.on("error",(error)=>{
+        console.log("Error: ",error);
+        throw error;            
+    });
+
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(` Server is running at port: ${process.env.PORT || 8000}`);
+    });
+})
+.catch((err)=>{
+    console.log("MONGODB connection failed !!!",err);
+})
