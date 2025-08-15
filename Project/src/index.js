2a5d8f68ffeee12fdd6879917b8bceb88a41846c
berserkcoder <shreyanshagrawalae2@gmail.com>
2025-08-15 15:41:45 +0530

    connect mongodb

---
+++ Project/src/index.js
@@ -0,0 +1,11 @@
+// require('dotenv').config({path: './env'})
+import dotenv from 'dotenv';
+import mongoose from "mongoose";
+import express from "express";
+import connectDB from './db/index.js';
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
