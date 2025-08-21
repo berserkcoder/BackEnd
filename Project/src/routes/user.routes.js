6d2b3c323c9bdd3e7b647481df3a3986d3fd0525
berserkcoder <shreyanshagrawalae2@gmail.com>
2025-08-21 10:10:07 +0530

    user controller done

--- Project/src/routes/user.routes.js
+++ Project/src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, registerUser,logoutUser,refreshAccessToken,changeCurrentPassword,getCurrentUser } from "../controllers/user.controllers.js";
+import {registerUser,loginUser,logoutUser,refreshAccessToken,changeCurrentPassword,getCurrentUser,updateAccountDetails,updateUserAvatar,updateUserCoverImage,getUserChannelProfile}  from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.middlewares.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
