8dd678bae0100485864c4c7e01e74a632b04ca68
berserkcoder <shreyanshagrawalae2@gmail.com>
2025-08-21 17:53:35 +0530

    all remaining models created

--- Project/src/controllers/user.controllers.js
+++ Project/src/controllers/user.controllers.js
@@ -167,6 +167,9 @@ const logoutUser = asyncHandler(async(req,res)=>{
     await User.findByIdAndUpdate(
         req.user._id,
         {
+            // $unset: {
+            //     refreshToken: 1
+            // }
             $set: {
                 refreshToken : undefined
             }
--- Project/src/routes/user.routes.js
+++ Project/src/routes/user.routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import {registerUser,loginUser,logoutUser,refreshAccessToken,changeCurrentPassword,getCurrentUser,updateAccountDetails,updateUserAvatar,updateUserCoverImage,getUserChannelProfile}  from "../controllers/user.controllers.js";
+import {registerUser,loginUser,logoutUser,refreshAccessToken,changeCurrentPassword,getCurrentUser,updateAccountDetails,updateUserAvatar,updateUserCoverImage,getUserChannelProfile,getWatchHistory}  from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.middlewares.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
+// import { verify } from "jsonwebtoken";
 
 const router = Router();
 
@@ -23,5 +24,19 @@ router.route("/login").post(loginUser)
 router.route("/logout").post(verifyJWT,logoutUser)
 
 router.route("/refresh-token").post(refreshAccessToken)
+
+router.route("/change-password").post(verifyJWT,changeCurrentPassword)
+
+router.route("/current-user").get(verifyJWT,getCurrentUser)
+
+router.route("update-account").patch(verifyJWT,updateAccountDetails)
+
+router.route("/change-avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
+
+router.route("/change-coverImage").patch(verifyJWT,upload.single("coverImage"),updateUserCoverImage)
+
+router.route("/channel/:username").get(verifyJWT,getUserChannelProfile)
+
+router.route("/watchHistory").get(verifyJWT,getWatchHistory)
 
 export default router; // default is used so we can give a username in other files accoring to out wishes
